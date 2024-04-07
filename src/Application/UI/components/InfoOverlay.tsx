aec323e1255da90f4a2fdcbb4bc631a6fff0d7f7
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-04-07 16:01:23 -0700

    update

--- src/Application/UI/components/InfoOverlay.tsx
+++ src/Application/UI/components/InfoOverlay.tsx
@@ -6,8 +6,8 @@ interface InfoOverlayProps {
     visible: boolean;
 }
 
-const NAME_TEXT = 'Henry Heffernan';
-const TITLE_TEXT = 'Software Engineer';
+const NAME_TEXT = 'Henry Wang';
+const TITLE_TEXT = 'Bay Area High Schooler';
 const MULTIPLIER = 1;
 
 const InfoOverlay: React.FC<InfoOverlayProps> = ({ visible }) => {
--- src/Application/UI/components/LoadingScreen.tsx
+++ src/Application/UI/components/LoadingScreen.tsx
@@ -161,7 +161,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                         <div style={styles.logoContainer}>
                             <div>
                                 <p style={styles.green}>
-                                    <b>Heffernan,</b>{' '}
+                                    <b>Wang,</b>{' '}
                                 </p>
                                 <p style={styles.green}>
                                     <b>Henry Inc.</b>
@@ -170,11 +170,11 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                         </div>
                         <div style={styles.headerInfo}>
                             <p>Released: 01/13/2000</p>
-                            <p>HHBIOS (C)2000 Heffernan Henry Inc.,</p>
+                            <p>HHBIOS (C)2000 Wang Henry Inc.,</p>
                         </div>
                     </div>
                     <div style={styles.body} className="loading-screen-body">
-                        <p>HSP S13 2000-2022 Special UC131S</p>
+                        <p>HSP S13 2000-2024 Special UC131S</p>
                         <div style={styles.spacer} />
                         {showBiosInfo && (
                             <>
@@ -207,7 +207,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                             <p>
                                 All Content Loaded, launching{' '}
                                 <b style={styles.green}>
-                                    'Henry Heffernan Portfolio Showcase'
+                                    'Henry Wang Portfolio Showcase'
                                 </b>{' '}
                                 V1.0
                             </p>
@@ -239,7 +239,7 @@ const LoadingScreen: React.FC<LoadingProps> = () => {
                     <p>But do enjoy what I have done so far :)</p>
                     <div style={styles.spacer} />
                     <div style={styles.spacer} /> */}
-                    <p>Henry Heffernan Portfolio Showcase 2022</p>
+                    <p>Henry Wang Portfolio Showcase</p>
                     {mobileWarning && (
                         <>
                             <br />
