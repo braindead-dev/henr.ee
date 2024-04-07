f1125deb61fcaeca5bc1dfa792a8c8d46c93966a
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-04-07 16:30:46 -0700

    d

--- server/index.ts
+++ server/index.ts
@@ -21,46 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-// Handle GET requests to /api route
-app.post('/api/send-email', (req, res) => {
-    const { name, company, email, message } = req.body;
-
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        auth: {
-            user: process.env.FOLIO_EMAIL,
-            pass: process.env.FOLIO_PASSWORD,
-        },
-    });
-
-    transporter
-        .verify()
-        .then(() => {
-            transporter
-                .sendMail({
-                    from: `"${name}" <henryheffernan.folio@gmail.com>`, // sender address
-                    to: 'henryheffernan@gmail.com, henryheffernan.folio@gmail.com', // list of receivers
-                    subject: `${name} <${email}> ${
-                        company ? `from ${company}` : ''
-                    } submitted a contact form`, // Subject line
-                    text: `${message}`, // plain text body
-                })
-                .then((info) => {
-                    console.log({ info });
-                    res.json({ message: 'success' });
-                })
-                .catch((e) => {
-                    console.error(e);
-                    res.status(500).send(e);
-                });
-        })
-        .catch((e) => {
-            console.error(e);
-            res.status(500).send(e);
-        });
-});
-
 // listen to app on port 8080
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
